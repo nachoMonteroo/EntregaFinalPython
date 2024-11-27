82122fe8a9c6c33bdc3c0c23479e6ba4ef19b5a3
Juan Ignacio Montero <montero.juanignacio@gmail.com>
2024-11-26 23:54:18 -0300

    todo lo que llegue de la tercera preentrega. Falta arreglar algunos errores, terminar de conectar las paginas y agregar los formularios

--- aplicacion/urls.py
+++ aplicacion/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from aplicacion import views    
 
 urlpatterns = [
-    path("", views.vista)
+    path("", views.inicio),
+    path("profesores/", views.profesores),
+    path("estudiantes/",views.estudiantes),
 ]
--- aplicacion/views.py
+++ aplicacion/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def vista(req):
+def inicio(req):
     return render(req, "aplicacion/index.html")
+
+
+def estudiantes(req):
+    return render(req, "aplicacion/estudiantes.html")
+
+
+def profesores(req):
+    return render(req, "aplicacion/profesores.html")
